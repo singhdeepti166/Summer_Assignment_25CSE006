7927c5051254b3e4593adb833921f3c7cf8ab5b8
Deepti <singhdeepti166@gmail.com>
2026-06-14 11:04:33 +0530

    question7.c

---
+++ Day 02/question7.c
@@ -0,0 +1,15 @@
+//QUESTION NO 7
+#include<stdio.h>
+int main(){
+    int n,product=1;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    while(n>0){
+        product=product*(n%10);
+        n=n/10;
+    }
+    printf("product=%d",product);
+    return 0;
+
+}
