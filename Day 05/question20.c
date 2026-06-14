fd8a06fa371ed63ec7d2d706f82873817648d569
Deepti <singhdeepti166@gmail.com>
2026-06-14 11:22:36 +0530

    question 20

---
+++ Day 05/question20.c
@@ -0,0 +1,24 @@
+//QUESTION NO 20
+#include<stdio.h>
+int main(){
+    int n,i,j,prime,largest=0;
+    printf("Enter number:");
+    scanf("%d",&n);
+
+    for(i=2;i<=n;i++){
+        if(n%i==0){
+           prime=1;
+
+           for(j=2;j<i;j++){
+               if(i%j==0){
+                   prime=0;
+                   break;
+               }
+           }
+           if(prime==1)
+           largest=i;
+        }
+    }
+    printf("largest prime number=%d",largest);  
+    return 0;
+}
