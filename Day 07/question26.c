03a4dc9be0d19f8d61a230abfe1d4765a7799259
Deepti <singhdeepti166@gmail.com>
2026-06-14 11:35:45 +0530

    question 26

---
+++ Day 07/question26.c
@@ -0,0 +1,16 @@
+//QUESTION NO 26
+#include <stdio.h>
+int fib(int n){
+    if(n <= 1)
+        return n;
+  return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    int n;
+    printf("Enter term: ");
+    scanf("%d", &n);
+
+    printf("%d", fib(n));
+    return 0;
+}
