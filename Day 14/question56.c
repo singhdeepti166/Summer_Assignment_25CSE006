101cf5467b1ccea69c45441384a79ba150941f69
Deepti <singhdeepti166@gmail.com>
2026-06-14 13:34:14 +0530

    question 56

---
+++ Day 14/question56.c
@@ -0,0 +1,22 @@
+//QUESTION NO 56
+#include<stdio.h>
+int main(){
+    int arr[100],n,i,j;
+    printf("Enter size:");
+    scanf("%d",&n);
+    
+    printf("Enter elements:\n");
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    
+    printf("Duplicate elements are:\n");
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
+                printf("%d\n",arr[i]);
+                break;
+            }
+        }
+    }
+       return 0;
+}
