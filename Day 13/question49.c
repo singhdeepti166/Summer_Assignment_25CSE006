0bb32bf3e0a2f89b3a6633468d69e6eb6206bf75
Deepti <singhdeepti166@gmail.com>
2026-06-14 12:10:07 +0530

    question 49

---
+++ Day 13/question49.c
@@ -0,0 +1,17 @@
+//QUESTION NO 49
+#include<stdio.h>
+int main(){
+    int arr[100],n,i;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter %d elements:\n",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Array elements are:\n");
+    for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
+
