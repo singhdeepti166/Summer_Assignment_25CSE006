40a4dea6e74e413637f7d20141394add6bc0ad11
Deepti <singhdeepti166@gmail.com>
2026-06-21 14:41:28 +0530

    question 82

---
+++ Day 21/question82.c
@@ -0,0 +1,19 @@
+//QUESTION NO 82
+#include<stdio.h>
+int main(){
+    char str[100],temp;
+    int i,len=0;
+    printf("Enter string:");
+    scanf("%s",str);
+    while(str[len]!='\0')
+    len++;
+    for(i=0;i<len/2;i++){
+        temp=str[i];
+        str[i]=str[len-1-i];
+        str[len-1-i]=temp;
+    }
+    printf("Reverse string:%s",str);
+    return 0;
+}
+
+
