548d56d9869e0d760affdc2d54cf92c297d636c9
Deepti <singhdeepti166@gmail.com>
2026-06-22 13:26:01 +0530

    question 87

---
+++ Day 22/question87.c
@@ -0,0 +1,20 @@
+//QUESTION NO 87
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100],ch;
+    int i,count=0;
+    printf("Enter a string:");
+    fgets(str,sizeof(str),stdin);
+    printf("Enter characters:");
+    scanf("%c",&ch);
+    for(i=0;str[i]!='\0';i++){
+        if(str[i]==ch)
+            count++;
+        }
+        printf("Frequency of %c=%d",ch,count);
+        return 0;
+    }
+    
+
+
