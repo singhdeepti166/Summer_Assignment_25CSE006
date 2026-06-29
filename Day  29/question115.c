0f7fc2d529a7c01ccd0482dc11f473b7e1687007
Deepti <singhdeepti166@gmail.com>
2026-06-29 13:44:34 +0530

    question 115

---
+++ Day  29/question115.c
@@ -0,0 +1,76 @@
+//QUESTION NO !!%
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp;
+    int choice, i, len;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    do {
+        printf("\n===== STRING OPERATIONS MENU =====\n");
+        printf("1. Find Length\n");
+        printf("2. Copy String\n");
+        printf("3. Concatenate Strings\n");
+        printf("4. Compare Strings\n");
+        printf("5. Reverse String\n");
+        printf("6. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+
+            case 1:
+                printf("Length = %lu\n", strlen(str1));
+                break;
+
+            case 2:
+                strcpy(str2, str1);
+                printf("Copied String = %s\n", str2);
+                break;
+
+            case 3:
+                printf("Enter second string: ");
+                scanf("%s", str2);
+
+                strcat(str1, str2);
+                printf("Concatenated String = %s\n", str1);
+                break;
+
+            case 4:
+                printf("Enter second string: ");
+                scanf("%s", str2);
+
+                if(strcmp(str1, str2) == 0)
+                    printf("Strings are Equal\n");
+                else
+                    printf("Strings are Not Equal\n");
+                break;
+
+            case 5:
+                len = strlen(str1);
+
+                for(i = 0; i < len / 2; i++) {
+                    temp = str1[i];
+                    str1[i] = str1[len - i - 1];
+                    str1[len - i - 1] = temp;
+                }
+
+                printf("Reversed String = %s\n", str1);
+                break;
+
+            case 6:
+                printf("Exiting Program...\n");
+                break;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+
+    } while(choice != 6);
+
+    return 0;
+}
